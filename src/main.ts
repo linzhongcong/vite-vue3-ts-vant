53ed46edb29a4af3dd145b5e3ec597799f32654e
alincong <1820716762@qq.com>
2022-01-06 01:19:33 +0800

    1/6

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router/index";
 import Vant from "vant";
 import "vant/lib/index.css";
+// flexible插件（响应式布局）
+import "lib-flexible/flexible";
 
 const app = createApp(App);
 app.use(router);
